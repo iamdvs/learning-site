d334fe67b6afe0d217d48f71e3493cf7da13afe0
iamdvs <programmersharp1379@gmail.com>
2019-06-30 19:47:16 +0430

    create verification module for verify your acount

--- learning/settings.py
+++ learning/settings.py
@@ -166,3 +166,10 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL='index'
 
 LOGIN_URL='login'
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='programmersharp1379@gmail.com'
+EMAIL_HOST_PASSWORD='hitsxwrtprogwmzx'
